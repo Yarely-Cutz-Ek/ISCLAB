4e8f0ff25e8d628826f91a3b6d05bca691c1d505
Yarely-Cutz-Ek <yarely.19070025@itsmotul.edu.mx>
2023-02-25 11:33:29 -0600

    Primera parte

---
+++ vue/vue-project/src/components/Texto.vue
@@ -0,0 +1,11 @@
+
+<template>
+    
+    <CENTER>
+
+ <font size="18" color="Black " font-style="italic"> BIENVENIDO AL LABORATORIO DE SISTEMAS Y APLICACIONES WEB  </font>
+    </CENTER>
+</template>
+    
+<style>
+</style>
---
+++ vue/vue-project/src/components/pregunta.vue
@@ -0,0 +1,24 @@
+<template>
+
+
+<center>
+<font size="30" color=#010055>¿Qué hacemos en isclab?</font>
+<p>
+    <font color=#7400AA>° Desarrollamos aplicaciones con fines de práctica, investigación y responsabilidad social.</font>
+</p>
+<p>
+    <font size="30" color=#010055>¿Tienes un proyecto en mente?</font>
+</p>
+
+<p>
+<font color=#7400AA>° Compártelo con nosotros y buscaremos la manera de ayudarte.</font>
+</p>
+
+</center>
+</template>
+
+
+<style>
+
+</style>
+
